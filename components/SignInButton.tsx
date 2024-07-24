87c9fab6746e02479f17d2e0af3fc8ccc0cb0243
DragonSenses <kendrickfrancisco@gmail.com>
2024-07-24 11:58:47 -0700

    feat: Create SignInButton.tsx

---
+++ components/SignInButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Button } from '@/components/ui/button';
+
+export default function SignInButton() {
+  return (
+    <Button variant="secondary" size="lg">
+      Sign In
+    </Button>
+  )
+}
