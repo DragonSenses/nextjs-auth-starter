59e5c9374742d6a229f465bf3301d11f51316126
DragonSenses <kendrickfrancisco@gmail.com>
2024-07-24 11:35:48 -0700

    feat: Improve AuthIntro structure & flexibility
    - Use semantic HTML elements `<header>` for improved accessibility and SEO
    - Introduce props (heading and description) to make the component more flexible
    - Update default values for heading and description

--- components/AuthIntro.tsx
+++ components/AuthIntro.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
-export default function AuthIntro() {
-  const heading: string = "Auth 🛡️";
+interface AuthIntroProps {
+  heading?: string;
+  description?: string;
+}
 
+export default function AuthIntro({
+  heading = "Auth 🛡️",
+  description = "An authentication service that shields your identity with the power of Auth.js",
+}: AuthIntroProps) {
   return (
-    <div>
-      <h1 className="text-6xl text-white font-semibold drop-shadow-md">
+    <header className="text-center text-white">
+      <h1 className="text-6xl font-semibold drop-shadow-md">
         {heading}
       </h1>
-      <p className="text-lg text-white">
-        An authentication service that shields your identity with the power of Auth.js
+      <p className="text-lg">
+        {description}
       </p>
-    </div>
-  )
-}
+    </header>
+  );
+};
