530daec361a0c969974f76f5b71acbe2c284b2dd
DragonSenses <kendrickfrancisco@gmail.com>
2024-07-24 11:27:39 -0700

    feat: Create AuthIntro.tsx

---
+++ components/AuthIntro.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function AuthIntro() {
+  const heading: string = "Auth 🛡️";
+
+  return (
+    <div>
+      <h1 className="text-6xl text-white font-semibold drop-shadow-md">
+        {heading}
+      </h1>
+      <p className="text-lg text-white">
+        An authentication service that shields your identity with the power of Auth.js
+      </p>
+    </div>
+  )
+}
