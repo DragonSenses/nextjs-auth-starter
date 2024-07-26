2de4c3662a580630c284d386f4648816c88eb3a2
DragonSenses <kendrickfrancisco@gmail.com>
2024-07-26 12:12:29 -0700

    feat: Define prop types for CardWrapper

--- components/auth/CardWrapper.tsx
+++ components/auth/CardWrapper.tsx
@@ -2,7 +2,21 @@
 
 import React from 'react';
 
-export default function CardWrapper() {
+interface CardWrapperProps {
+  children: React.ReactNode;
+  backButtonHref: string;
+  backButtonLabel: string;
+  headerLabel: string;
+  showSocial?: boolean;
+};
+
+export default function CardWrapper({
+  children,
+  backButtonHref,
+  backButtonLabel,
+  headerLabel,
+  showSocial,
+}: CardWrapperProps) {
   return (
     <div>CardWrapper</div>
   )
