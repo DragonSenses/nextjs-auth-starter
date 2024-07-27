74e424423bf2cd32d09c399c817984eb6288f556
DragonSenses <kendrickfrancisco@gmail.com>
2024-07-27 14:58:57 -0700

    feat: Add Google social sign-in button

--- components/auth/SocialSignIn.tsx
+++ components/auth/SocialSignIn.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import GoogleIcon from '@/public/img/auth/GoogleIcon';
+import { Button } from '@/components/ui/button';
+
 export default function SocialSignIn() {
   return (
-    <div>SocialSignIn</div>
-  )
+    <div className='w-full flex items-center gap-x-2'>
+      <Button
+        size='lg'
+        className='w-full'
+        variant='outline'
+      >
+        <GoogleIcon className='h-5 w-5'/>
+      </Button>
+    </div>
+  );
 }
