0d5852e1079f837ac20cf8f0c0e05670ffab94c2
DragonSenses <kendrickfrancisco@gmail.com>
2024-07-30 15:20:41 -0700

    feat: Define the sign-in form with useForm hook
    - Mark as client component
    - Import zodResolver, useForm, z, and SignInSchema
    - Implement the sign-in form logic

--- components/auth/SignInForm.tsx
+++ components/auth/SignInForm.tsx
@@ -1,7 +1,24 @@
+"use client";
+
 import React from 'react';
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { SignInSchema } from '@/schemas';
 import CardWrapper from '@/components/auth/CardWrapper';
 
 export default function SignInForm() {
+
+  // 1. Define the sign-in form.
+  const form = useForm<z.infer<typeof SignInSchema>>({
+    resolver: zodResolver(SignInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <CardWrapper
       backButtonHref="/auth/register"
