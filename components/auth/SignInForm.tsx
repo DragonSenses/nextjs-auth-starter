3a2dfea2e950530352463391170e022724073a4e
DragonSenses <kendrickfrancisco@gmail.com>
2024-08-01 15:29:52 -0700

    feat: Render FormError component in SignInForm

--- components/auth/SignInForm.tsx
+++ components/auth/SignInForm.tsx
@@ -7,6 +7,7 @@ import { z } from "zod"
 
 import { SignInSchema } from '@/schemas';
 import CardWrapper from '@/components/auth/CardWrapper';
+import FormError from '@/components/form/FormError';
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -73,6 +74,7 @@ export default function SignInForm() {
               )}
             />
           </div>
+          <FormError />
           <Button type="submit" className='w-full bg-sky-500'>
             Sign In
           </Button>
