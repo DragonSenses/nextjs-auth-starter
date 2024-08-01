2c7927de7f96fd9e55230d8ebe01927b105b2921
DragonSenses <kendrickfrancisco@gmail.com>
2024-08-01 14:47:03 -0700

    feat(SignInSchema): Strengthen password validation
    - Increase minimum length to 14 characters
    - Set maximum length to 32 characters
    - Enforce requirements for at least one of each: an uppercase letter, a lowercase letter, a number, and a special character.
    - Ensure password is not empty

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,10 +1,15 @@
-import { z } from "zod"
+import { z } from 'zod';
 
 export const SignInSchema = z.object({
   email: z.string().email({
-    message: "Please enter a valid email address."
+    message: 'Please enter a valid email address.',
   }),
-  password: z.string().min(1, {
-    message: "Password must be at least 14 characters long."
-  }),
+  password: z
+    .string()
+    .min(14, 'Password must be at least 14 characters long')
+    .max(32, 'Password must be a maximum of 32 characters')
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+={[}]|:;\"'<,>.])[A-Za-z\d!@#$%^&*()_+={[}]|:;\"'<,>.]{14,}$/)
+    .refine((value) => value.length > 0, {
+      message: 'Password is required',
+    }),
 });
