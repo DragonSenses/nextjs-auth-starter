3f65f486b24733c3c39334784173fe966e1c6d9b
DragonSenses <kendrickfrancisco@gmail.com>
2024-08-05 16:49:50 -0700

    feat: Define the sign-up form with useForm hook
    - Mark as client component
    - Import zodResolver, useForm, z, and SignInSchema
    - Use the `useForm` hook from "react-hook-form" to create a form instance.
    - Set up form validation, default values, and other configuration options.

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,6 +1,24 @@
+"use client";
+
 import React from 'react';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { SignUpSchema } from '@/schemas';
 
 export default function SignUpPage() {
+
+  // 1. Define the sign-up form.
+  const form = useForm<z.infer<typeof SignUpSchema>>({
+    resolver: zodResolver(SignUpSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <div>SignUpPage</div>
   )
