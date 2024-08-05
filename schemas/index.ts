5ca847cc59af99fbef6b4beb36999cc9cb0b824f
DragonSenses <kendrickfrancisco@gmail.com>
2024-08-05 16:12:00 -0700

    feat: Define sign-up schema using zod

--- schemas/index.ts
+++ schemas/index.ts
@@ -15,3 +15,22 @@ export const SignInSchema = z.object({
       message: 'Password is required',
     }),
 });
+
+export const SignUpSchema = z.object({
+  email: z.string().email({
+    message: 'Please enter a valid email address.',
+  }),
+  password: z
+    .string()
+    .min(14, 'Password must be at least 14 characters long')
+    .max(32, 'Password must be a maximum of 32 characters')
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+={[}]|:;\"'<,>.])[A-Za-z\d!@#$%^&*()_+={[}]|:;\"'<,>.]{14,}$/, {
+      message: 'Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character.',
+    })
+    .refine((value) => value.length > 0, {
+      message: 'Password is required',
+    }),
+  name: z.string().min(1, {
+    message: 'Please enter a valid name',
+  }),
+});
