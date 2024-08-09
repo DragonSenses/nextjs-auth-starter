bbb0eabf2dd1e025557363eef1dad77a504743c6
DragonSenses <kendrickfrancisco@gmail.com>
2024-08-09 16:30:07 -0700

    feat: Improve error handling in getUserByEmail

--- utils/getUserByEmail.ts
+++ utils/getUserByEmail.ts
@@ -1,7 +1,16 @@
+import { Prisma } from "@prisma/client";
 import prisma from "@/db/prismaSingleton";
 
+/**
+ * Retrieves a user by their email address.
+ *
+ * @param email - The email address to search for.
+ * @returns The user object if found, or null if not found or an error occurs
+ * @see {@link https://www.prisma.io/docs/orm/reference/error-reference} Prisma error reference
+ */
 export default async function getUserByEmail(email: string) {
   try {
+    // Check if an existing user with the given email exists
     const user = await prisma.user.findUnique({
       where: {
         email,
@@ -9,7 +18,14 @@ export default async function getUserByEmail(email: string) {
     });
 
     return user;
-  } catch {
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      // Handle specific Prisma errors (e.g., database connection issue)
+      console.error("Prisma error:", error.message);
+    } else {
+      // Handle other unexpected errors
+      console.error("Unexpected error:", error);
+    }
     return null;
   }
 }
