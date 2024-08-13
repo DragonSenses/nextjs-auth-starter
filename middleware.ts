43ff73b1f8be678b4060c182ae670275c7209b60
DragonSenses <kendrickfrancisco@gmail.com>
2024-08-12 20:36:37 -0700

    feat: Configure selective middleware paths

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,19 @@
-export { auth as middleware } from "@/auth"
+export { auth as middleware } from "@/auth";
+
+import { auth } from "@/auth";
+
+export default auth((req) => {
+  // req.auth
+});
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  /*
+   * Match all request paths except for the ones starting with:
+   * - api (API routes)
+   * - _next/static (static files)
+   * - _next/image (image optimization files)
+   * - favicon.ico (favicon file)
+   */
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
