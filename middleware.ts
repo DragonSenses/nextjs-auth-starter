e0e07149c04a9ec5ad41ec9f3754fb9783f52922
DragonSenses <kendrickfrancisco@gmail.com>
2024-08-13 19:42:24 -0700

    feat: Configure middleware paths
    Specify regular expressions to match request paths, excluding Next.js internals and all static files (unless found in search params). Always run for API routes.

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,15 @@
-export { auth as middleware } from "@/auth";
-
 import { auth } from "@/auth";
 
 export default auth((req) => {
   // req.auth
+  console.log("ROUTE: ", req.nextUrl.pathname);
 });
 
-// Optionally, don't invoke Middleware on some paths
-export const config = {
-  /*
-   * Match all request paths except for the ones starting with:
-   * - api (API routes)
-   * - _next/static (static files)
-   * - _next/image (image optimization files)
-   * - favicon.ico (favicon file)
-   */
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
