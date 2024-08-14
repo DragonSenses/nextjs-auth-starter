9d0afe2ddcf9f8492774018b686b999d8a903e92
DragonSenses <kendrickfrancisco@gmail.com>
2024-08-13 22:40:46 -0700

    feat(auth): Configure NextAuth with PrismaAdapter
    - Create a separate Auth.js instance using the configuration from auth.config.ts `auth.config.ts`
    - Add the Prisma Adapter
    - Add JWT session strategy

--- auth.ts
+++ auth.ts
@@ -1,6 +1,13 @@
-import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub],
+import NextAuth from "next-auth"
+import authConfig from "./auth.config"
+ 
+import { PrismaClient } from "@prisma/client"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+ 
+const prisma = new PrismaClient()
+ 
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
