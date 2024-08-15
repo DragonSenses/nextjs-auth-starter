05c27e1a072779f9e4e857064e576b36d55f5c46
DragonSenses <kendrickfrancisco@gmail.com>
2024-08-14 18:40:51 -0700

    feat(routes): Define global array for publicRoutes

---
+++ routes.ts
@@ -0,0 +1,9 @@
+/**
+ * An array of public routes accessible to all users.
+ * These routes do not require authentication.
+ * @type {string[]}
+ */
+export const publicRoutes: string[] = [
+  "/",
+];
+
