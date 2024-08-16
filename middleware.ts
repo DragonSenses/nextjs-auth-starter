b601c86e6b2c4dfb5c17e8602f2ccd63c51bf582
DragonSenses <kendrickfrancisco@gmail.com>
2024-08-15 21:08:28 -0700

    feat(auth): Allow api auth routes in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,13 @@
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
+import { apiAuthRoute } from "@/routes";
  
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
   console.log("ROUTE: ", req.nextUrl.pathname);
+
+  const isApiAuthRoute = req.nextUrl.pathname.startsWith(apiAuthRoute);
 });
 
 const config = {
