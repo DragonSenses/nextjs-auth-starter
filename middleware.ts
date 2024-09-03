cfdf6edf356ffd5f105954baad6531e5071ff5a4
DragonSenses <kendrickfrancisco@gmail.com>
2024-09-02 21:54:50 -0700

    feat(middleware): Remove database functionality
    - Removed reliance on database functionality in middleware to support Edge Runtime.
    - Middleware no longer fetches session or user details.
    - Implemented route protection checks instead.

--- middleware.ts
+++ middleware.ts
@@ -1,42 +1,8 @@
-import { NextRequest, NextResponse } from "next/server";
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
-import {
-  apiAuthRoute, 
-  DEFAULT_SIGNIN_REDIRECT,
-  protectedRoutes, 
-  publicRoutes 
-} from "@/routes";
 
-// Use only one of the two middleware options below
 // 1. Use middleware directly
-// export const { auth: middleware } = NextAuth(authConfig)
- 
-// 2. Wrapped middleware option
-const { auth } = NextAuth(authConfig);
-
-export default auth(async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-
-  console.log("ROUTE: ", pathname);
-
-  const isApiAuthRoute = pathname.startsWith(apiAuthRoute);
-  const isPublicRoute = publicRoutes.includes(pathname);
-  const isProtectedRoute = protectedRoutes.includes(pathname);
-
-  if (isApiAuthRoute || isPublicRoute) {
-    // Allow access to /api/auth routes and public routes
-    return NextResponse.next();
-  }
-
-  if (isProtectedRoute) {
-    // Redirect to sign-in page if not authenticated
-    return NextResponse.redirect(DEFAULT_SIGNIN_REDIRECT);
-  }
-
-  // Allow every other route
-  return NextResponse.next();
-});
+export const { auth: middleware } = NextAuth(authConfig)
 
 const config = {
   matcher: [
