22c64a135dba113823fd591607048a0a4b5b37b4
DragonSenses <kendrickfrancisco@gmail.com>
2024-09-03 19:24:46 -0700

    feat: Add route protection check for SignUpPage

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { auth } from '@/auth';
 import SignUpForm from '@/components/auth/SignUpForm';
 
-export default function SignUpPage() {
+export default async function SignUpPage() {
+  const session = await auth();
+
+  if (!session) {
+    return <div>Not authenticated</div>
+  }
+
   return (
     <SignUpForm />
   )
