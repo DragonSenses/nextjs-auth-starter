7d214809b566199761d6048a66d28c0da6bc386a
DragonSenses <kendrickfrancisco@gmail.com>
2024-09-03 20:00:01 -0700

    feat(signin): Render Redirect on session existence
    Implemented logic to render the Redirect component in SignInPage when a session exists. This ensures users are redirected to the appropriate page if they are already signed in.

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { auth } from "@/auth"
 
+import { DEFAULT_SIGNIN_REDIRECT } from '@/routes';
+import Redirect from '@/components/auth/Redirect';
 import SignInForm from '@/components/auth/SignInForm';
 
 export default async function SignInPage() {
   const session = await auth()
 
-  if (!session) {
-    return <div>Not authenticated</div>
+  if (session) {
+    return <Redirect to={DEFAULT_SIGNIN_REDIRECT} />;
   }
 
   return (
