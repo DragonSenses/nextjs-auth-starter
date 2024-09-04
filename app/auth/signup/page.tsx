a544f63e7e1efaa55d37fa17c37f1efd45582276
DragonSenses <kendrickfrancisco@gmail.com>
2024-09-03 19:56:25 -0700

    feat(signup): Render Redirect on session existence

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { auth } from '@/auth';
+import { DEFAULT_SIGNIN_REDIRECT } from '@/routes';
 import SignUpForm from '@/components/auth/SignUpForm';
+import Redirect from '@/components/auth/Redirect';
 
 export default async function SignUpPage() {
   const session = await auth();
 
-  if (!session) {
-    return <div>Not authenticated</div>
+  if (session) {
+    return <Redirect to={DEFAULT_SIGNIN_REDIRECT} />;
   }
 
   return (
