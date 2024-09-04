516ef48c35cbe67bf90ad13de5640dda93123180
DragonSenses <kendrickfrancisco@gmail.com>
2024-09-04 15:25:14 -0700

    refactor(SignUpPage): dynamic Redirect rendering
    Dynamically import and render the Redirect component in SignUpPage to handle client-side redirection. This ensures compatibility with server-side rendering and improves performance.

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { auth } from '@/auth';
+import dynamic from 'next/dynamic';
+
+// Dynamically import the client component
+const Redirect = dynamic(() => import('@/components/auth/Redirect'), { ssr: false });
+
 import { DEFAULT_SIGNIN_REDIRECT } from '@/routes';
 import SignUpForm from '@/components/auth/SignUpForm';
-import Redirect from '@/components/auth/Redirect';
 
 export default async function SignUpPage() {
   const session = await auth();
