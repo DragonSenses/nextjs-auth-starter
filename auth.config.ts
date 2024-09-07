3d55d65d78c9599ecbb1682fc39331a4d642ddec
DragonSenses <kendrickfrancisco@gmail.com>
2024-09-06 20:40:37 -0700

    feat: Add credentials provider in auth.config.ts
    - Imported CredentialsProvider from next-auth/providers/credentials
    - Added CredentialsProvider to the providers array
    - Configured the authorize function to validate user credentials

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,36 @@
-import GitHub from "next-auth/providers/github"
-import type { NextAuthConfig } from "next-auth"
- 
+import type { NextAuthConfig } from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
+
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [GitHub],
-} satisfies NextAuthConfig
+  providers: [
+    GitHub,
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        let user = null;
+
+        // logic to salt and hash password
+        const pwHash = saltAndHashPassword(credentials.password);
+
+        // logic to verify if the user exists
+        user = await getUserFromDb(credentials.email, pwHash);
+
+        if (!user) {
+          // No user found, so this is their first attempt to login
+          // meaning this is also the place you could do registration
+          throw new Error("User not found.");
+        }
+
+        // return user object with their profile data
+        return user;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
